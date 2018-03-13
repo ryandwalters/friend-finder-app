7ecdd1cf895988dc4fbc3d00258bf1480da04a8a
ryandwalters <ryandwalters@gmail.com>
2018-03-13 03:41:28 -0500

    commit

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ require("./routing/htmlRoutes.js")(app);
 
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
